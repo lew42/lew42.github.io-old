0682a4c08006331d1a3a3dbc3b71a068f9cc9649
Michael Lewis <mike@lew42.com>
2017-12-09 12:04:55 -0600

    build

--- define.js
+++ define.js
@@ -1,395 +1,429 @@
-// 1_define_root.js
-define = function(){
-	return define.define.apply(define, arguments);
+define = function(...args){
+	return new define.Module(...args);
 };
 
+define.path = "modules";
 
-
-// 2_assign.js
-define.assign = function(){
-	var arg;
-	for (var i = 0; i < arguments.length; i++){
-		arg = arguments[i];
-		for (var prop in arg){
-			this[prop] = arg[prop];
-		}
-	}
-	return this;
-};
-
-
-
-// 3_Base.js
-;(function(define, assign){
-
-	var Base = define.Base = function(){
-		this.instantiate.apply(this, arguments);
-	};
-
-	Base.assign = Base.prototype.assign = assign;
-
-	Base.prototype.instantiate = function(){};
-
-	Base.assign({
-		extend: function(){
-			var Ext = function(){
-				return this.instantiate.apply(this, arguments);
-			};
-			Ext.assign = this.assign;
-			Ext.assign(this);
-			Ext.prototype = Object.create(this.prototype);
-			Ext.prototype.constructor = Ext;
-			Ext.prototype.assign.apply(Ext.prototype, arguments);
-
-			return Ext;
-		}
-		
+define.P = function(){
+	var resolve, reject;
+	
+	const p = new Promise(function(res, rej){
+		resolve = res;
+		reject = rej;
 	});
 
-})(define, define.assign);
+	p.resolve = resolve;
+	p.reject = reject;
 
+	return p;
+};
 
-
-
-// 4_logger.js
-(function(define){
-
-	var console_methods = ["log", "group", "debug", "trace", 
-		"error", "warn", "info", "time", "timeEnd", "dir"];
-
-	var g = function(str, fn){
-		this.group(str);
-		fn();
-		this.end();
-	};
-
-	var gc = function(str, fn){
-		this.groupc(str);
-		fn();
-		this.end();
-	};
-
-	var make_enabled_logger = function(){
-		var enabled_logger = console.log.bind(console);
-		enabled_logger.enabled = true;
-		enabled_logger.disabled = false;
-
-		console_methods.forEach(function(name){
-			enabled_logger[name] = console[name].bind(console);
-		});
-
-		enabled_logger.groupc = console.groupCollapsed.bind(console);
-		enabled_logger.end = console.groupEnd.bind(console);
-		enabled_logger.close = function(closure, ctx){
-			if (typeof closure === "function"){
-				closure.call(ctx);
-			}
-			this.end();
-		};
-
-		enabled_logger.g = g;
-		enabled_logger.gc = gc;
-
-		enabled_logger.isLogger = true;
-		
-		return enabled_logger;
-	};
-
-
-	var noop = function(){};
-
-	var make_disabled_logger = function(){
-		var disabled_logger = function(){};
-		disabled_logger.disabled = true;
-		disabled_logger.enabled = false;
-		console_methods.forEach(function(name){
-			disabled_logger[name] = noop;
-		});
-		disabled_logger.groupc = noop;
-		disabled_logger.end = noop;
-		disabled_logger.close = function(closure, ctx){
-			if (typeof closure === "function"){
-				closure.call(ctx);
-			}
-		};
-		disabled_logger.g = g;
-		disabled_logger.gc = gc;
-
-		disabled_logger.isLogger = true;
-		
-		return disabled_logger;
-	};
-
-	var enabled_logger = make_enabled_logger();
-	var disabled_logger = make_disabled_logger();
-
-	enabled_logger.on = disabled_logger.on = enabled_logger;
-	enabled_logger.off = disabled_logger.off = disabled_logger;
-
-	var logger = define.logger = function(value){
-		if (typeof value === "function" && value.isLogger){
+define.logger = (function(){
+	const logger = function(value){
+		if (typeof value === "function" && value.logger){
 			return value;
 		} else if (value){
-			return enabled_logger;
+			return logger.active;
 		} else {
-			return disabled_logger;
+			return logger.inactive;
 		}
 	};
 
-	define.log = logger(false);
-	define.debug = logger(false);
 
-})(define)
+	const noop = function(){};
 
-
-
-// 5_Module.js
-;(function(define, Base, log, debug){
-
-	var Module = define.Module = Base.extend({
-		instantiate: function(id){
-			this.id = id;
-			
-			this.log = define.log;
-			this.debug = define.debug;
-
-			this.dependencies = []; // dependencies (Module instances)
-			this.dependents = [];
-
-			this.views = [];
-		},
-		fixLoggers: function(){
-			if (typeof this.log === "boolean"){
-				this.log = this.log ? define.log.on : define.log.off;
-			}
-
-			if (typeof this.debug === "boolean"){
-				this.debug = this.debug ? define.log.on : define.log.off;
-			}
-		},
-		define: function(args){
-			this.assign(args);
-			this.fixLoggers();
-			// this.log = logger(this.log);
-			// this.debug = logger(this.debug); //?
-
-			this.debug.group("define", this.id, this.deps || []);
-
-			if (this.deps)
-				this.deps.forEach(this.require.bind(this));
-
-			this.defined = true;
-
-			this.exec();
-
-			this.debug.end();
-		},
-		require: function(id){
-			var module = define.get(id);
-
-			// all deps
-			this.dependencies.push(module);
-
-			// deps track dependents, too
-			module.dependents.push(this);
-
-			if (this.debug.enabled){
-				module.log = this.debug;
-				module.debug = this.debug;
-			}
-		},
-		/**
-		 * All requests get delayed.  See define(), define.delayRequests(), and define.requests()
-		 */
-		request: function(){
-			if (!this.defined && !this.requested){
-				this.script = document.createElement("script");
-				this.script.src = define.resolve(this.id);
-
-				// used in global define() function as document.currentScript.module
-				this.script.module = this;
-
-				this.debug("request", this.id);
-				this.requested = true;
-				document.head.appendChild(this.script);
-			}
-		},
-		path: function(){
-			var a = document.createElement("a");
-			a.href = this.script.src;
-			if (this.requested){
-			}
-		},
-		exec: function(){
-			this.debug.group("ping", this.id);
-
-			if (this.executed){
-				this.debug("already executed");
-				// this happens when the "finish" loop has a dependency that will be pinged later in the loop, but also gets pinged earlier in the loop, due to multiple dependents
-					// essentially, if b depends on a, and c depends on a, but c also depends on b, then what happens is
-					// a finishes
-					// a begins finish loop to notify b and c
-					// b is notified first, and when complete, pings c
-					// when c is pinged, at this point, a is finished, so c is executed synchronously
-					// after c's finish loop, we fall back to the original a.finish that we started with
-					// in a's finish loop, we just pinged b, now we ping c
-					// here's where the second .exec came in
-				// i originally thought that .exec wouldn't be called twice
-				// but, unless I setTimeout 0 on the .execs...
-				return false;
-			}
-
-			var args = this.args();
-			
-			if (args){
-				this.log.group(this.id, this.dependencies.map(function(dep){ return dep.id }));
-				this.value = this.factory.apply(this, args);
-				this.executed = true;
-				// !this.dependents.length && 
-				this.log.end();
-				this.finish();
-			} else {
-				this.debug(this.id, "not ready");
-			}
-			this.debug.end();
-		},
-		finish: function(){
-			this.debug.group(this.id, "finished");
-			for (var i = 0; i < this.dependents.length; i++){
-				this.debug("dependent", this.dependents[i].id, "exec()");
-				this.dependents[i].exec();
-			}
-			this.debug.end();
-			this.finished = true;
-		},
-		args: function(){
-			var dep, args = [];
-			for (var i = 0; i < this.dependencies.length; i++){
-				dep = this.dependencies[i];
-				if (dep.executed){
-					args.push(dep.value);
-				} else {
-					this.debug("awaiting", dep.id);
-					return false;
-				}
-			}
-			return args;
-		},
-		render: function(){
-			var view;
-			if (this.View){
-				view = new this.View({
-					module: this
-				});
-				this.views.push(view);
-				return view;
-			}
-			return false;
-
-			// then
-
-			this.views.forEach((view) => view.update());
-		}
-	});
-
-})(define, define.Base, define.log, define.debug);
-
-
-
-// 6_define.js
-;(function(define, assign, Module, log){
+	const active = logger.active = console.log.bind(console);
+	const inactive = logger.inactive = function(){};
 	
-	define.assign({
-		Module: Module,
-		log: log,
-		debug: log.off,
-		modules: {},
-		moduleRoot: "modules",
-		define: function(){
-			var args = define.args(arguments);
-			var script = document.currentScript; 
-			var module;
-			var a;
+	const console_methods = ["log", "group", "groupCollapsed", "groupEnd", "debug", "trace", "error", "warn", "info", "time", "timeEnd", "dir"];
+	
+	for (const method of console_methods){
+		active[method] = console[method].bind(console);
+		inactive[method] = noop;
+	}
 
-			if (args.id){
-				module = define.get(args.id);
-			} else if (script && script.module){
-				module = script.module;
-			} else {
-				a = document.createElement("a");
-				a.href = script.src;
-				module = new define.Module(a.pathname);
-			}
+	// some references
+	active.logger = inactive.logger = logger;
+	active.active = inactive.active = active;
+	inactive.inactive = active.inactive = inactive;
 
-			define.delayRequests();
+	// some flags
+	active.is_active = true;
+	inactive.is_active = false;
+	
+	// alias these long methods
+	active.groupc = console.groupCollapsed.bind(console);
+	inactive.groupc = noop;
+	active.end = console.groupEnd.bind(console);
+	inactive.end = noop;
 
-			return module.define(args);
-		},
-		delayRequests: function(){
-			define.debug.time("define.requests timeout");
-			if (define.delayRequestsTimeout){
-				clearTimeout(define.delayRequestsTimeout);
-			}
-			define.delayRequestsTimeout = setTimeout(define.requests, 0);
-		},
-		get: function(id){
-			return (define.modules[id] = define.modules[id] || new define.Module(id));
-		},
-		args: function(argu){
-			var arg, args = {};
-			for (var i = 0; i < argu.length; i++){
-				arg = argu[i];
-				if (typeof arg === "string")
-					args.id = arg;
-				else if (toString.call(arg) === '[object Array]')
-					args.deps = arg;
-				else if (typeof arg === "function")
-					args.factory = arg;
-				else if (typeof arg === "object")
-					assign.call(args, arg); /// NOTE: external reference to `assign()`
-				else if (typeof arg === "undefined")
-					continue;
-				else
-					console.error("whoops");
-			}
-			return args;
-		},
-		requests: function(){
-			define.debug.g("define.requests", function(){
-				define.debug.timeEnd("define.requests timeout");
-				for (var i in define.modules){
-					define.modules[i].request();
+	return logger;
+})();
+
+define.Base = class Base {
+
+	constructor(){
+		this.events = {};
+	}
+
+	// get log(){
+	// 	return this._log || define.logger();	
+	// }
+
+	// set log(value){
+	// 	this._log = define.logger(value);
+	// }
+
+	set_log(value){
+		this.log = define.logger(value);
+	}
+
+	on(event, cb){
+		var cbs = this.events[event];
+		if (!cbs)
+			cbs = this.events[event] = [];
+		cbs.push(cb);
+		return this;
+	}
+
+	emit(event, ...args){
+		const cbs = this.events[event];
+		if (cbs && cbs.length)
+			for (const cb of cbs)
+				cb.apply(this, ...args);
+		return this;
+	}
+
+	off(event, cbForRemoval){
+		const cbs = this.events[event];
+		if (cbs)
+			for (var i = 0; i < cbs.length; i++)
+				if (cbs[i] === cbForRemoval)
+					cbs.splice(i, 1);
+		return this;
+	}
+	
+	assign(...args){
+		for (arg of args)
+			for (const prop in arg)
+				this[prop] = arg[prop];
+		return this;
+	}
+
+	set(...args){
+		if (this._set)
+			this._set(...args); // pre .set() hook
+
+		for (const arg of args){
+			// pojo arg
+			if (arg && arg.constructor === Object){
+
+				// iterate over arg props
+				for (var j in arg){
+
+					// set_*
+					if (this["set_" + j]){
+						this["set_" + j](arg[j]);
+						// create a .set_assign() method that simply calls assign with the arg...
+
+					// "assign" prop will just call assign
+					} else if (j === "assign") {
+						this.assign(arg[j]);
+
+					} else if (this[j] && this[j].set){
+						this[j].set(arg[j]);
+
+					// existing prop is a pojo - "extend" it
+					} else if (this[j] && this[j].constructor === Object){
+
+						// make sure its safe
+						if (this.hasOwnProperty(j))
+							set.call(this[j], arg[j]);
+
+						// if not, protect the prototype
+						else {
+							this[j] = set.call(Object.create(this[j]), arg[j]);
+						}
+
+					// everything else, assign
+					} else {
+						// basically just arrays and fns...
+						// console.warn("what are you", arg[j]);
+						this[j] = arg[j];
+					}
 				}
-			});
-		},
-		resolve: function(id){
-			var parts = id.split("/"); // id could be //something.com/something/?
 
-			// change this to default mimic
-			// require "thing" --> thing/thing.js
-			// require "thing.js" --> thing.js
-			// require "thing/" --> thing/index?
-			
-			// "thing/"
-			if (id[id.length-1] === "/"){
-				// ends in "/", mimic last part --> "thing/thing.js"
-				id = id + parts[parts.length-2] + ".js";
+			// non-pojo arg
+			} else if (this.set$){
+				// auto apply if arg is array?
+				this.set$(arg);
 
-			// does not contain ".js", "thing"
-			} else if (parts[parts.length-1].indexOf(".js") < 0){
-				id = id + "/" + parts[parts.length-1] + ".js";
+			// oops
+			} else {
+				console.warn("not sure what to do with", arg);
 			}
-
-			// convert non-absolute paths to moduleRoot paths
-			if (id[0] !== "/"){
-				id = "/" + define.moduleRoot + "/" + id;
-			}
-
-			return id;
 		}
-	});
 
-})(define, define.assign, define.Module, define.log);
+		if (this.set_)
+			this.set_(...args); // post .set() hook
+
+		return this; // important
+	}
+};
+
+define.Module = class Module extends define.Base {
+
+	constructor(...args){
+		super();
+		return (this.get(args[0]) || this.initialize()).set(...args);
+	}
+
+	get(token){
+		return typeof token === "string" && Module.get(this.resolve(token));
+	}
+
+	initialize(...args){
+		this.ready = Module.P();
+		this.dependencies = [];
+		this.dependents = [];
+
+		return this; // see instantiate()
+	}
+
+	exec(){
+		this.exports = {};
+
+		// log if no dependents
+		const log = define.logger(!this.dependents.length);
+		
+		log.group(this.id);
+		const ret = this.factory.call(this, this.require.bind(this), this.exports, this);
+		log.end();
+
+		if (typeof ret !== "undefined")
+			this.exports = ret;
+
+		return this.exports;
+	}
+
+	// `this.token` is transformed into `this.id`
+	resolve(token){ 
+		var id, 
+			parts;
+
+		// mimic the base path
+		if (token === "."){
+			if (!this.url)
+				throw "don't define with relative tokens";
+
+			parts = this.url.path.split("/");
+			id = this.url.path + parts[parts.length-2] + ".js";
+		} else {
+			parts = token.split("/");
+
+			// token ends with "/", ex: "path/thing/"
+			if (token[token.length-1] === "/"){
+				// repeat last part, ex: "path/thing/thing.js"
+				id = token + parts[parts.length-2] + ".js";
+
+			// last part doesn't contain a ".", ex: "path/thing"
+			} else if (parts[parts.length-1].indexOf(".") < 0){
+				// repeat last part, add ".js", ex: "path/thing/thing.js"
+				id = token + "/" + parts[parts.length-1] + ".js";
+			} else {
+				id = token;
+			}
+
+			if (id.indexOf("./") === 0){
+				if (!this.url)
+					throw "don't define with relative tokens";
+				id = this.url.path + id.replace("./", "");
+				// token = token.replace("./", ""); // nope - need to parse id->host/path
+			} else if (id[0] !== "/"){
+				id = "/" + define.path + "/" + id;
+			}
+		}
+
+		return id;
+	}
+
+	import(token){
+		return (new this.constructor(this.resolve(token))).register(this);
+	}
+
+	register(dependent){
+		this.dependents.push(dependent);
+
+		if (!this.factory && !this.queued && !this.requested){
+			this.queued = setTimeout(this.request.bind(this), 0);
+		}
+
+		return this.ready; // see import()
+	}
+
+	require(token){
+		const module = this.get(token);
+		if (!module)
+			throw "module not preloaded";
+		return module.exports;
+	}
+
+	request(){
+		this.queued = false;
+		
+		if (this.factory)
+			throw "request wasn't dequeued";
+
+		if (!this.requested){
+			this.script = document.createElement("script");
+			this.src = this.id;
+			this.script.src = this.src;
+			document.head.appendChild(this.script);
+			this.requested = true;
+		} else {
+			throw "trying to re-request?"
+		}
+	}
+
+	set_id(id){
+		if (this.id && this.id !== id)
+			throw "do not reset id";
+
+		if (!this.id){
+			this.id = id;
+			this.url = Module.url(this.id);
+
+			// cache me
+			Module.set(this.id, this);
+		} else {
+			// this.id && this.id === id
+			// noop is ok
+		}
+	}
 
 
+	set_token(token){
+		this.token = token;
+		this.set_id(this.resolve(this.token));
+	}
+	
+	set_deps(deps){
+		if (this.factory)
+			throw "provide deps before factory fn";
 
+		this.deps = deps;
+	}
+
+	set_factory(factory){
+		if (this.factory)
+			throw "don't re-set factory fn";
+
+		this.factory = factory;
+
+		this.deps = this.deps || [];
+
+		// for anonymous modules (no id)
+		this.id_from_src();
+
+		// all the magic, right here
+		this.ready.resolve(
+			Promise.all(this.deps.map(dep => this.import(dep)))
+				.then(args => this.exec.apply(this, args))
+		);
+
+
+		if (this.queued)
+			clearTimeout(this.queued);
+	}
+
+	id_from_src(){
+		if (!this.id){
+			const a = document.createElement("a");
+			a.href = document.currentScript.src;
+
+			this.set({
+				id: a.pathname,
+				log: true // might need to be adjusted
+			})
+		}
+	}
+
+	// set(value) is forwarded here, when value is non-pojo 
+	set$(arg){
+		if (typeof arg === "string")
+			this.set_token(arg);
+		else if (toString.call(arg) === '[object Array]')
+			this.set_deps(arg);
+		else if (typeof arg === "function")
+			this.set_factory(arg);
+		else if (typeof arg === "object")
+			this.assign(arg);
+		else if (typeof arg === "undefined"){
+			// I think I had an acceptable use case for this, can't remember when it happens
+			console.warn("set(undefined)?");
+		}
+		else
+			console.error("whoops");
+
+		return this;
+	}
+
+	static P(){
+		var resolve, reject;
+		
+		const p = new Promise(function(res, rej){
+			resolve = res;
+			reject = rej;
+		});
+
+		p.resolve = resolve;
+		p.reject = reject;
+
+		return p;
+	}
+
+	static get(id){
+		if (!this.modules)
+			this.modules = {};
+		return this.modules[id]
+	}
+
+	static set(id, module){
+		if (!this.modules)
+			this.modules = {};
+		if (this.modules[id])
+			throw "don't redefine a module";
+		this.modules[id] = module;
+	}
+
+	static doc(...cbs){
+		if (!this.document_ready){
+			this.document_ready = new Promise((res, rej) => {
+				if (/comp|loaded/.test(document.readyState))
+					res();
+				else
+					document.addEventListener("DOMContentLoaded", res);
+			});
+		}
+
+		return this.document_ready.then(...cbs);
+	}
+
+	static base(base){
+		if (base) this._base = base;
+		return this._base || "modules";
+	}
+
+	static url(original){
+		const a = document.createElement("a");
+		a.href = original;
+		return {
+			original: original,
+			url: a.href,
+			host: a.host,
+			hostname: a.hostname,
+			pathname: a.pathname,
+			path: a.pathname.substr(0, a.pathname.lastIndexOf('/') + 1)
+		};
+	}
+}
